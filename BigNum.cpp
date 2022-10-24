cea894a98605ddadaf93ac03a3c879a198bc3d30
IvanYachUkr <vanya.yach2004@gmail.com>
2022-10-24 21:30:27 +0300

    structured document, added << operator overload

--- BigNum.cpp
+++ BigNum.cpp
@@ -6,6 +6,173 @@
 #include <regex>
 #include "BigNum.h"
 
+//Overloaded operators
+Big_num Big_num::operator+(const Big_num &second_term) const {
+    Big_num n;
+    std::string first_number = num_decimal;
+    std::string second_number = second_term.num_decimal;
+
+    std::string res = sum_big_num(first_number,second_number);
+
+    n.num_decimal = res;
+
+    return n;
+}
+
+
+Big_num Big_num::operator-(const Big_num &second_term) const {
+    Big_num n;
+    std::string first_number = num_decimal;
+    std::string second_number = second_term.num_decimal;
+
+    n.num_decimal = subtract_big_num(first_number, second_number);
+
+    return n;
+}
+
+std::ostream& operator<<(std::ostream& os, const Big_num& bm)
+{
+    os << bm.num_decimal;
+    return os;
+}
+
+// End of downloaded operatords
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//+ - Operations
+std::string Big_num::sum_big_num(std::string first_number,std::string second_number){
+    int len_first_num = first_number.length();
+    int len_second_num = second_number.length();
+
+    if (len_first_num > len_second_num)
+        swap(first_number, second_number);
+
+    std::string res;
+
+    reverse(first_number.begin(), first_number.end());
+    reverse(second_number.begin(), second_number.end());
+
+    int carry = 0;
+    for (int i = 0; i < len_first_num; i++) {
+
+        //to convert a digit to its corresponding character use + '0'
+        //to covert a character of the digit to the digit subtract '0'
+
+        int sum = ((first_number[i] - '0') + (second_number[i] - '0') + carry);
+        res.push_back(sum % 10 + '0');
+
+        carry = sum / 10;
+    }
+
+    for (int i = len_first_num; i < len_second_num; i++) {
+        int sum = ((second_number[i] - '0') + carry);
+        res.push_back(sum % 10 + '0');
+        carry = sum / 10;
+    }
+
+    if (carry)
+        res.push_back(carry + '0');
+
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+
+
+std::string Big_num::subtract_big_num(std::string first_number, std::string second_number) {
+    std::string res;
+
+
+    int len_first_num = first_number.length(), len_second_num = second_number.length();
+
+
+    reverse(second_number.begin(), second_number.end());
+
+    int carry = 0;
+
+    for (int i = 0; i < len_second_num; i++) {
+
+        int subtract
+                = ((first_number[i] - '0')
+                   - (second_number[i] - '0')
+                   - carry);
+
+        if (subtract < 0) {
+            subtract = subtract + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        res.push_back(subtract + '0');
+    }
+
+    for (int i = len_second_num; i < len_first_num; i++) {
+        int subtract = ((first_number[i] - '0') - carry);
+
+        if (subtract < 0) {
+            subtract = subtract + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        res.push_back(subtract + '0');
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
+// End of +- operation
+
+
+
+
+
+
+
+
+
+
+// support functions
+std::string Big_num::convert_decimal_to_binary(std::string num_decimal) {
+
+    return {};
+}
+
+
+
+std::string Big_num::remove_leading_zeros(const std::string& num) {
+    // Use regular expression to remove leading 0s
+    const std::regex pattern("^0+(?!$)");
+
+    // Replaces the matched value
+    // with given string
+    std::string num_without_leading_zeros = regex_replace(num, pattern, "");
+    return num_without_leading_zeros;
+}
+// End of support functions
+
+
+
+
+
+
+
+
+
 
 
 int Big_num::toom_cook_method_multiplication(std::string first_num, std::string second_num) {
@@ -21,7 +188,7 @@ int Big_num::shtrassen_shenhage_method_multiplication(std::string first_num, std
 }
 
 //std::basic_string<char> Big_num::karatsuba_algorithm_multiplication(Big_num first_n, Big_num second_n) {
-std::basic_string<char> Big_num::karatsuba_algorithm_multiplication(std::string first_n, std::string second_n) {
+std::basic_string<char> Big_num::karatsuba_algorithm_multiplication(std::string first_n, std::string second_n) const {
 
 //    std::string first_num =  first_n.num_decimal;
 //    std::string second_num = second_n.num_decimal;
@@ -99,127 +266,13 @@ std::basic_string<char> Big_num::karatsuba_algorithm_multiplication(std::string 
 
 }
 
-std::string Big_num::sum_big_num(std::string first_number,std::string second_number){
-    int len_first_num = first_number.length();
-    int len_second_num = second_number.length();
-
-    if (len_first_num > len_second_num)
-        swap(first_number, second_number);
-
-    std::string res;
-
-    reverse(first_number.begin(), first_number.end());
-    reverse(second_number.begin(), second_number.end());
-
-    int carry = 0;
-    for (int i = 0; i < len_first_num; i++) {
-
-        //to convert a digit to its corresponding character use + '0'
-        //to covert a character of the digit to the digit subtract '0'
-
-        int sum = ((first_number[i] - '0') + (second_number[i] - '0') + carry);
-        res.push_back(sum % 10 + '0');
-
-        carry = sum / 10;
-    }
-
-    for (int i = len_first_num; i < len_second_num; i++) {
-        int sum = ((second_number[i] - '0') + carry);
-        res.push_back(sum % 10 + '0');
-        carry = sum / 10;
-    }
-
-    if (carry)
-        res.push_back(carry + '0');
-
-    reverse(res.begin(), res.end());
-    return res;
-}
 
 
-Big_num Big_num::operator+(const Big_num &second_term) const {
-    Big_num n;
-    std::string first_number = num_decimal;
-    std::string second_number = second_term.num_decimal;
-
-    std::string res = sum_big_num(first_number,second_number);
-
-    n.num_decimal = res;
-
-    return n;
-}
 
 
-Big_num Big_num::operator-(const Big_num &second_term) const {
-    Big_num n;
-    std::string first_number = num_decimal;
-    std::string second_number = second_term.num_decimal;
-
-    n.num_decimal = subtract_big_num(first_number, second_number);
-
-    return n;
-}
 
 
-std::string Big_num::convert_decimal_to_binary(std::string num_decimal) {
 
-    return {};
-}
-
-std::string Big_num::subtract_big_num(std::string first_number, std::string second_number) {
-    std::string res;
-
-
-    int len_first_num = first_number.length(), len_second_num = second_number.length();
-
-
-    reverse(second_number.begin(), second_number.end());
-
-    int carry = 0;
-
-    for (int i = 0; i < len_second_num; i++) {
-
-        int subtract
-                = ((first_number[i] - '0')
-                   - (second_number[i] - '0')
-                   - carry);
-
-        if (subtract < 0) {
-            subtract = subtract + 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
-
-        res.push_back(subtract + '0');
-    }
-
-    for (int i = len_second_num; i < len_first_num; i++) {
-        int subtract = ((first_number[i] - '0') - carry);
-
-        if (subtract < 0) {
-            subtract = subtract + 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
-
-        res.push_back(subtract + '0');
-    }
-
-    reverse(res.begin(), res.end());
-    return res;
-}
-
-std::string Big_num::remove_leading_zeros(const std::string& num) {
-    // Use regular expression to remove leading 0s
-    const std::regex pattern("^0+(?!$)");
-
-    // Replaces the matched value
-    // with given string
-    std::string num_without_leading_zeros = regex_replace(num, pattern, "");
-    return num_without_leading_zeros;
-}
 
 
 
--- BigNum.h
+++ BigNum.h
@@ -9,19 +9,20 @@
 #include <string>
 #include <utility>
 
-class BigNum_Abstract {
-public:
-    std::string num;
-    virtual ~BigNum_Abstract() = default;
-    //virtual std::string convert_decimal_to_binary(std::string num_decimal) = 0;
-    //virtual int karatsuba_algorithm_multiplication(Big_num first_num, Big_num second_num) = 0;
-    virtual std::basic_string<char> karatsuba_algorithm_multiplication(std::string first_num, std::string second_num) = 0;
-    virtual int toom_cook_method_multiplication(std::string first_num, std::string second_num) = 0;
-    virtual  int shenhage_method_multiplication(std::string first_num, std::string second_num) = 0;
-    virtual int shtrassen_shenhage_method_multiplication(std::string first_num, std::string second_num) = 0;
-};
+//class BigNum_Abstract {
+//public:
+//    std::string num;
+//    virtual ~BigNum_Abstract() = default;
+//    //virtual std::string convert_decimal_to_binary(std::string num_decimal) = 0;
+//    //virtual int karatsuba_algorithm_multiplication(Big_num first_num, Big_num second_num) = 0;
+//    virtual std::basic_string<char> karatsuba_algorithm_multiplication(std::string first_num, std::string second_num) = 0;
+//    virtual int toom_cook_method_multiplication(std::string first_num, std::string second_num) = 0;
+//    virtual  int shenhage_method_multiplication(std::string first_num, std::string second_num) = 0;
+//    virtual int shtrassen_shenhage_method_multiplication(std::string first_num, std::string second_num) = 0;
+//};
 
-class Big_num : public BigNum_Abstract {
+//class Big_num : public BigNum_Abstract {
+class Big_num{
 public:
     std::string num_decimal;
     std::string num_binary;
@@ -32,6 +33,7 @@ public:
 
     Big_num operator + (Big_num const &second_term) const;
     Big_num operator - (Big_num const &second_term) const;
+    Big_num operator << (Big_num const &second_term) const;
     static std::string convert_decimal_to_binary(std::string num_decimal);
     //std::basic_string<char> karatsuba_algorithm_multiplication(Big_num first_num, Big_num second_num) override;
 
@@ -39,10 +41,14 @@ public:
     static std::string subtract_big_num(std::string first_number,std::string second_number);
 
     static std::string remove_leading_zeros(const std::string& num);
-    std::basic_string<char> karatsuba_algorithm_multiplication(std::string first_num, std::string second_num) override;
-    int toom_cook_method_multiplication(std::string first_num, std::string second_num) override;
-    int shenhage_method_multiplication(std::string first_num, std::string second_num) override;
-    int shtrassen_shenhage_method_multiplication(std::string first_num, std::string second_num) override;
+//    std::basic_string<char> karatsuba_algorithm_multiplication(std::string first_num, std::string second_num) override;
+//    int toom_cook_method_multiplication(std::string first_num, std::string second_num) override;
+//    int shenhage_method_multiplication(std::string first_num, std::string second_num) override;
+//    int shtrassen_shenhage_method_multiplication(std::string first_num, std::string second_num) override;
+    std::basic_string<char> karatsuba_algorithm_multiplication(std::string first_num, std::string second_num) const;
+    int toom_cook_method_multiplication(std::string first_num, std::string second_num);
+    int shenhage_method_multiplication(std::string first_num, std::string second_num);
+    int shtrassen_shenhage_method_multiplication(std::string first_num, std::string second_num);
 };
 
 
